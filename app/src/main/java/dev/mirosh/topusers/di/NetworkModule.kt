0e21f62bc3263dc46bd1199e73939231d17bb99f
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-04-20 01:06:24 +0700

    Set up basic Hilt Network Module

---
+++ app/src/main/java/dev/mirosh/topusers/di/NetworkModule.kt
@@ -0,0 +1,25 @@
+package dev.mirosh.topusers.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.mirosh.topusers.data.StackExchangeApi
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://api.stackexchange.com/2.2")
+            .build()
+    }
+
+    @Provides
+    fun provideStackExchangeApi(retrofit: Retrofit): StackExchangeApi {
+        return retrofit.create(StackExchangeApi::class.java)
+    }
+}
