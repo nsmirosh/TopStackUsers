d91d223ecd2f33d2911adcaa073d79ac8fb43487
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-04-23 14:17:48 +0700

    Started writing a test for GetTopUsersUseCase

---
+++ app/src/test/java/dev/mirosh/topusers/domain/GetTopUsersUseCaseTest.kt
@@ -0,0 +1,16 @@
+package dev.mirosh.topusers.domain
+
+import org.junit.Test
+
+class GetTopUsersUseCaseTest {
+
+
+    @Test
+    fun  getTopUsers_returnsSuccess() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+}
