6edc03a3d1294076baf987d38eace1acdaa07926
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-05-04 00:11:17 +0700

    Added singleton annotations to dependencies in modules to actually make dependencies singleton

--- app/src/main/java/dev/mirosh/topusers/di/NetworkModule.kt
+++ app/src/main/java/dev/mirosh/topusers/di/NetworkModule.kt
@@ -10,20 +10,22 @@ import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
     @Provides
+    @Singleton
     fun provideJonConverterFactory(): Converter.Factory {
         val json = Json { ignoreUnknownKeys = true }
         return json.asConverterFactory("application/json".toMediaType())
     }
 
     @Provides
+    @Singleton
     fun provideRetrofit(jsonConverterFactory: Converter.Factory): Retrofit {
-
         return Retrofit.Builder()
             .baseUrl("https://api.stackexchange.com/2.2/")
             .addConverterFactory(jsonConverterFactory)
@@ -31,6 +33,7 @@ object NetworkModule {
     }
 
     @Provides
+    @Singleton
     fun provideStackExchangeApi(retrofit: Retrofit): StackExchangeApi {
         return retrofit.create(StackExchangeApi::class.java)
     }
